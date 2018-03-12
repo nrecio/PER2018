119981533c76408c60654a17890c6fc438edb9c0
nrecio <37300095+nrecio@users.noreply.github.com>
2018-03-12 16:48:45 +0100

    Add files via upload

---
+++ L7-practicas/server3modf.py
@@ -0,0 +1,73 @@
+import http.server
+import socketserver
+import json
+
+# -- Puerto donde lanzar el servidor
+PORT = 8001
+
+
+# Clase con nuestro manejador. Es una clase derivada de BaseHTTPRequestHandler
+# Esto significa que "hereda" todos los metodos de esta clase. Y los que
+# nosotros consideremos los podemos reemplazar por los nuestros
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    # GET. Este metodo se invoca automaticamente cada vez que hay una
+    # peticion GET por HTTP. El recurso que nos solicitan se encuentra
+    # en self.path
+    def do_GET(self):
+
+        # La primera linea del mensaje de respuesta es el
+        # status. Indicamos que OK
+        self.send_response(200)
+
+        # En las siguientes lineas de la respuesta colocamos las
+        # cabeceras necesarias para que el cliente entienda el
+        # contenido que le enviamos (que sera json)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        # Este es el mensaje que enviamos al cliente: un texto y
+        # el recurso solicitado
+        if self.path == "/":
+            filename = "Datos.json"
+        else:
+            filename= "otro.json"
+        
+        print("Fichero a servir: {}".format(filename))
+        with open(filename, "r") as f:
+            contenido = f.read()
+
+        message = contenido
+      
+
+        # Enviar el mensaaje completo
+        self.wfile.write(bytes(message, "utf8"))
+        print("File served!")
+        return
+
+
+# ----------------------------------
+# El servidor comienza a aqui
+# ----------------------------------
+# Establecemos como manejador nuestra propia clase
+Handler = testHTTPRequestHandler
+
+# -- Configurar el socket del servidor, para esperar conexiones de clientes
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+
+# Entrar en el bucle principal
+# Las peticiones se atienden desde nuestro manejador
+# Cada vez que se ocurra un "GET" se invoca al metodo do_GET de
+# nuestro manejador
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("")
+    print("Interrumpido por el usuario")
+
+print("")
+print("Servidor parado")
+httpd.close()
+
+# https://github.com/joshmaker/simple-python-webserver/blob/master/server.py
